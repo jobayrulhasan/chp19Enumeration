175ab9af252cf76307f3b41d3f2c69125d698a66
unknown <jobayerdpi11@gmail.com>
2021-04-01 11:23:12 +0600

    Enumeration in chapter 19

---
+++ chap19Enumeration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap19Enumeration
+{
+    class Program
+    {
+        public static System.Collections.IEnumerable myNumber()
+        {
+            yield return 12;
+            yield return 22;
+            yield return 28;
+            yield return 25;
+            yield return 33;
+            yield return 40;
+            yield return 48;
+        }
+        static void Main(string[] args)
+        {
+            foreach (int  item in myNumber())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+      
+    }
+}
